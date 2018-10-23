644ada74346a5b9dc1f26e440f2f96a811b0473a
Enya Shen <enya_shen@163.com>
2018-10-23 16:54:42 +0800

    Data Table

---
+++ @type^react-table/README.md
@@ -0,0 +1,16 @@
+# Installation
+> `npm install --save @types/react-table`
+
+# Summary
+This package contains type definitions for react-table (https://github.com/react-tools/react-table).
+
+# Details
+Files were exported from https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/react-table
+
+Additional Details
+ * Last updated: Wed, 10 Oct 2018 22:44:29 GMT
+ * Dependencies: react
+ * Global values: none
+
+# Credits
+These definitions were written by Roy Xue <https://github.com/royxue>, Pavel Sakalo <https://github.com/psakalo>, Krzysztof Porębski <https://github.com/Havret>.
--- src/components/dataset-pane/index.tsx
+++ src/components/dataset-pane/index.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import * as CSSModules from "react-css-modules";
 import { connect } from "react-redux";
-// import { FieldList, PresetWildcardFieldList } from "./field-list";
 import { Dataset, State } from "../../models";
 import { VoyagerConfig } from "../../models/config";
 import { selectConfig, selectDataset } from "../../selectors/";
-// import { DataSelector } from "../data-selector";
 import * as styles from "./dataset-pane.scss";
+
+import ReactTable from "react-table";
+import * as _ from 'lodash';
 
 
 export interface DatasetPanelProps {
@@ -15,14 +16,26 @@ export interface DatasetPanelProps {
 }
 
 export class DatasetPaneBase extends React.PureComponent<DatasetPanelProps, {}> {
-  public render() {
-    // const { name } = this.props.data;
+  render() {
+    const { isLoading, data } = this.props.data;
+    if (isLoading) {
+      return null;
+    }
+
+    const headers = Object.keys(this.props.data.data.values[0]);
+    const columns = headers.map(x => ({Header: x, accessor: x}));
+    console.log('columns', columns);
 
     return (
-          <div>
-              数据
+          <div styleName="dataset-pane">
+              <ReactTable
+                data={data.values}
+                columns={columns}
+                defaultPageSize = {25}
+                pageSizeOptions = {[25, 50, 100]}
+              />
           </div>
-    );
+    )
   }
 }
 
